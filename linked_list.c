6a01130545139941c166248dffc78c90d012475c
TrueFalafel <jonuma18@gmail.com>
2017-03-13 16:58:11 +0000

    update linked_list

--- linked_list.c
+++ linked_list.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
 
+struct item{
+	data x;
+	item* next;
+}
+struct data{
+	int x;
+	int y;
+	int z;
+}
+
+item* list_init(){
+	return NULL;
+}
+
+void* list_append(item* root, data x){
+	item *new, *aux;
+
+	new = (item*)malloc(sizeof(item));
+	if(new == NULL){
+		printf("Erro na alocação\n", );
+		exit(-1);
+	}
+
+	new->x = x;
+	new->next = NULL;
+
+	if(root == NULL){
+		root = new;
+	}else{
+		aux = root;
+		while(aux->next != NULL)
+			aux = aux->next;
+
+		aux->next = new;
+	}
+
+	return;
+}
+
+void* list_remove(item* root, data x){
+	item *aux, *aux_seg;
+
+	if(root == NULL){
+		printf("Already an empty list!\n", );
+		return;
+	}
+
+	aux = root;
+	aux_seg = aux->next;
+	if(root->x == x){
+		root = root->next;
+		free(aux);
+	}else{
+		while(aux_seg->x != x){
+			if(aux_seg->next == NULL){
+				printf("No data x found!\n", );
+				return;
+			}
+			aux = aux->next;
+			aux_seg = aux_seg->next;
+		}
+		aux->next = aux_seg->next;
+		free(aux_seg);
+	}
+	return;
+}
--- linked_list.h
+++ linked_list.h
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linked_list.h"
 
 item* list_init();
 
-item* list_append(item* root, data x);
+void* list_append(item* root, data x);
 
-item* list_remove(item* root, data x);
+void* list_remove(item* root, data x);
 
-item* list_search(item* root, data x);
+void* list_search(item* root, data x);
 
-item* list_free(item* root);
-
-
+void* list_free(item* root);
