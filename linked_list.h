24649da926ab5ca07fc1ac6e3f03590222a5fa64
TrueFalafel <jonuma18@gmail.com>
2017-03-13 16:21:13 +0000

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+item* list_init();
+
+item* list_append(item* root, data x);
+
+item* list_remove(item* root, data x);
+
+item* list_search(item* root, data x);
+
+item* list_free(item* root);
+
 
