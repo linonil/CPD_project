f3b3269477329b51f6d2ab2e601bb7ccdbfdf8b6
TrueFalafel <jonuma18@gmail.com>
2017-03-13 18:02:09 +0000

    linked_list complete (i think)

--- linked_list.c
+++ linked_list.c
@@ -1,31 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "linked_list.h"
 
-struct item{
-	data x;
-	item* next;
-}
 struct data{
 	int x;
 	int y;
 	int z;
-}
+};
+
+struct item{
+	struct data K;
+	item* next;
+};
 
 item* list_init(){
 	return NULL;
 }
 
-void* list_append(item* root, data x){
+int equal_data(data K1, data K2){
+	if(K1.x == K2.x && K1.y == K2.y && K1.z == K2.z)
+		return 1;
+	else
+		return 0;
+}
+
+void* list_append(item* root, data K){
 	item *new, *aux;
 
 	new = (item*)malloc(sizeof(item));
 	if(new == NULL){
-		printf("Erro na alocação\n", );
+		printf("Erro na alocação\n");
 		exit(-1);
 	}
 
-	new->x = x;
+	new->K = K;
 	new->next = NULL;
 
 	if(root == NULL){
@@ -41,23 +47,23 @@ void* list_append(item* root, data x){
 	return;
 }
 
-void* list_remove(item* root, data x){
+void* list_remove(item* root, data K){
 	item *aux, *aux_seg;
 
 	if(root == NULL){
-		printf("Already an empty list!\n", );
+		printf("Already an empty list!\n");
 		return;
 	}
 
 	aux = root;
 	aux_seg = aux->next;
-	if(root->x == x){
+	if(equal_data(root->K, K)){
 		root = root->next;
 		free(aux);
 	}else{
-		while(aux_seg->x != x){
+		while(equal_data(aux_seg->K, K)){
 			if(aux_seg->next == NULL){
-				printf("No data x found!\n", );
+				printf("No data K found!\n");
 				return;
 			}
 			aux = aux->next;
@@ -65,6 +71,38 @@ void* list_remove(item* root, data x){
 		}
 		aux->next = aux_seg->next;
 		free(aux_seg);
+	}
+	return;
+}
+
+item* list_search(item* root, data K){
+	item *aux;
+
+	if(root == NULL){
+		printf("Search cancelled, empty list, returning NULL\n");
+		return NULL;
+	}
+
+	aux = root;
+	while(equal_data(aux->K, K) && aux != NULL)
+		aux = aux->next;
+
+	if(aux == NULL){
+		printf("Element not found, returning NULL\n");
+		return NULL;
+	}else{
+		return aux;
+	}
+
+}
+
+void* list_free(item* root){
+	item *aux;
+
+	while(root != NULL){
+		aux = root;
+		root = root->next;
+		free(aux);
 	}
 	return;
 }
--- linked_list.h
+++ linked_list.h
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linked_list.h"
+
+struct data;
+struct item;
+typedef struct data data;
+typedef struct item item;
 
 item* list_init();
 
-void* list_append(item* root, data x);
+void* list_append(item* root, data K);
 
-void* list_remove(item* root, data x);
+void* list_remove(item* root, data K);
 
-void* list_search(item* root, data x);
+item* list_search(item* root, data K);
 
 void* list_free(item* root);
